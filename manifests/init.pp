1dc223622d87c0cc8cfe6c0c895b5d98a3a30e20
atomless <james@atomless.com>
2014-01-27 14:43:59 +0000

    no comment

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@ class java {
   $jre_url = 'https://s3.amazonaws.com/boxen-downloads/java/jre-7u21-macosx-x64.dmg'
   $jdk_url = 'https://s3.amazonaws.com/boxen-downloads/java/jdk-7u21-macosx-x64.dmg'
   $wrapper = "${boxen::config::bindir}/java"
-  $sec_dir = '/Library/Java/JavaVirtualMachines/jdk1.7.0_21.jdk/Contents/Home/jre/lib/security'
+  #$sec_dir = '/Library/Java/JavaVirtualMachines/jdk1.7.0_21.jdk/Contents/Home/jre/lib/security'
 
   $java_url = "http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2013-05.dmg"
 
@@ -43,27 +43,27 @@ class java {
   # Allow 'large' keys locally.
   # http://www.ngs.ac.uk/tools/jcepolicyfiles
 
-  file { $sec_dir:
-    ensure  => 'directory',
-    owner   => 'root',
-    group   => 'wheel',
-    mode    => '0775',
-    require => Package['Java SE 6']
-  }
+  # file { $sec_dir:
+  #   ensure  => 'directory',
+  #   owner   => 'root',
+  #   group   => 'wheel',
+  #   mode    => '0775',
+  #   require => Package['Java SE 6']
+  # }
 
-  file { "${sec_dir}/local_policy.jar":
-    source  => 'puppet:///modules/java/local_policy.jar',
-    owner   => 'root',
-    group   => 'wheel',
-    mode    => '0664',
-    require => File[$sec_dir]
-  }
+  # file { "${sec_dir}/local_policy.jar":
+  #   source  => 'puppet:///modules/java/local_policy.jar',
+  #   owner   => 'root',
+  #   group   => 'wheel',
+  #   mode    => '0664',
+  #   require => File[$sec_dir]
+  # }
 
-  file { "${sec_dir}/US_export_policy.jar":
-    source  => 'puppet:///modules/java/US_export_policy.jar',
-    owner   => 'root',
-    group   => 'wheel',
-    mode    => '0664',
-    require => File[$sec_dir]
-  }
+  # file { "${sec_dir}/US_export_policy.jar":
+  #   source  => 'puppet:///modules/java/US_export_policy.jar',
+  #   owner   => 'root',
+  #   group   => 'wheel',
+  #   mode    => '0664',
+  #   require => File[$sec_dir]
+  # }
 }
